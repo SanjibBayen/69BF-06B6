6d6c4480d4755503e5d966a9d445e8747dc7fdbb
Ani Fly <anifly13@gmail.com>
2025-09-02 19:35:42 +0000

    still not included

--- src/components/mobile/mobile-home.tsx
+++ src/components/mobile/mobile-home.tsx
@@ -1,9 +1,9 @@
 
 
-import ActivityChart from "../dashboard/activity-chart";
 import HeartRateCard from "../dashboard/compact/heart-rate-card";
 import RiskIndicator from "../dashboard/risk-indicator";
 import SleepTrends from "../dashboard/sleep-trends";
+import Spo2Meter from "../dashboard/spo2-meter";
 
 
 export default function MobileHome() {
@@ -22,9 +22,7 @@ export default function MobileHome() {
                 <div className="sm:col-span-2">
                     <HeartRateCard />
                 </div>
-                <div className="sm:col-span-2">
-                    <ActivityChart />
-                </div>
+                 <Spo2Meter />
                 <div className="sm:col-span-2">
                     <SleepTrends />
                 </div>
